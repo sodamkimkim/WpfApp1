a4b0e576acbc6383ac456839a37b1ad49a9b7fc0
sodamkimkim <theka265@gmail.com>
2024-12-15 12:34:01 +0900

    practice_radiobutton group event , listview databinding

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1
 {
@@ -19,6 +20,26 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("?");
+        }
+
+        private void RadioBtnClick(object sender, RoutedEventArgs e)
+        {
+            var rbtn = sender as RadioButton;
+            if (rbtn != null) 
+                MessageBox.Show(rbtn.Name); 
+
+        }
+
+        private void listView1_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<User> userList = new List<User>() 
+            { new User("Adam", 1, "C:\\Users\\theka\\OneDrive\\Desktop\\WpfApp1_pr\\WpfApp1\\WpfApp1\\Resources\\Image1.png"), new User("Noah", 15, "C:\\Users\\theka\\OneDrive\\Desktop\\WpfApp1_pr\\WpfApp1\\WpfApp1\\Resources\\Image2.png") };
+            listView1.ItemsSource = userList;
         }
     }
 }
---
+++ WpfApp1/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class User
+    {
+		private string  name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int userAge;
+
+		public int UserAge
+		{
+			get { return userAge; }
+			set { userAge = value; }
+		}
+
+		private string userImage;
+
+
+        public string UserImage
+		{
+			get { return userImage; }
+			set { userImage = value; }
+		}
+        public User(string name, int userAge, string userImage)
+        {
+            Name = name;
+            UserAge = userAge;
+            UserImage = userImage;
+        }
+
+	} // end of class
+} // end of namespace
