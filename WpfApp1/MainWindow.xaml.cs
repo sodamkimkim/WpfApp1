b9bf639e58481f9de3a92387625bc7c544f9e4b9
sodamkimkim <theka265@gmail.com>
2024-12-16 11:25:43 +0900

    절대경로로 삽입

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System.Net;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp1.Models;
 
 namespace WpfApp1
@@ -35,12 +26,13 @@ namespace WpfApp1
 
             userA.Name = "Noah";
             userA.UserAge = 15;
-            userA.UserImg = "C:\\WpfApp1\\WpfApp1\\Resources\\Image1.png";
+            //       userA.UserImg = "C:\\Users\\user\\OneDrive\\바탕 화면\\WpfApp1\\WpfApp1\\Resources\\Image1.png";
+            userA.UserImg = "Resources/Image1.png";
 
             User userB = new User();
             userB.Name = "Adam";
             userB.UserAge = 15;
-            userB.UserImg = "C:\\WpfApp1\\WpfApp1\\Resources\\Image1.png";
+            userB.UserImg = "Resources/Image1.png";
 
             myList1.Add(userA);
             myList1.Add(userB);
@@ -50,9 +42,10 @@ namespace WpfApp1
         private void RadioBtnGroup_Checked(object sender, RoutedEventArgs e)
         {
             var radioButton = sender as RadioButton;
-            if (radioButton != null) {
+            if (radioButton != null)
+            {
                 MessageBox.Show($"Selected : {radioButton.Content}", "Selection Changed");
-                    }
+            }
         }
 
     }
